f1d0edad40ce3f31a5aec9c2f042e01d2f240f46
Boran Seckin <boran@boranseckin.com>
2022-11-08 15:59:33 -0500

    Separate grep into its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,62 @@
 use std::{ env, fs, error::Error };
 
+mod modules;
+pub use crate::modules::grep;
+
+pub enum Functions {
+  Grep
+}
+
 pub struct Config {
+  pub function: Functions
+}
+
+impl Config {
+  pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    if args.len() < 2 {
+      return Err("not enough arguments");
+    }
+
+    match args[1].as_str() {
+      "grep" => Ok(Config { function: Functions::Grep }),
+      _ => Err("unknown function")
+    }
+  }
+}
+
+pub struct GrepConfig {
   pub query: String,
   pub file_path: String,
   pub ignore_case: bool,
 }
 
-impl Config {
-  pub fn build(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
+impl GrepConfig {
+  pub fn build(args: &[String]) -> Result<GrepConfig, &'static str> {
+    if args.len() < 4 {
+      return Err("not enough argument for grep");
+    }
 
-      let query = args[1].clone();
-      let file_path = args[2].clone();
+    let query = args[2].clone();
+    let file_path = args[3].clone();
 
-      let ignore_case = env::var("IGNORE_CASE").is_ok();
-  
-      Ok(Config { query, file_path, ignore_case })
+    let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+    Ok(GrepConfig { query, file_path, ignore_case })
   }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run_grep(config: GrepConfig) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.file_path)?;
 
   let results = if config.ignore_case {
-    search_case_insensitive(&config.query, &contents)
+    grep::search_case_insensitive(&config.query, &contents)
   } else {
-    search(&config.query, &contents)
+    grep::search(&config.query, &contents)
   };
 
   for line in results {
     println!("{line}");
   }
 
-  Ok(())
-}
-
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-
-  results
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let query = query.to_lowercase();
-
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-
-  results
-}
-
-#[cfg(test)]
-mod test {
-  use super::*;
-
-  #[test]
-  fn case_sensitive() {
-    let query = "duct";
-    let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-  }
-
-  #[test]
-  fn case_insensitive() {
-    let query = "rUsT";
-    let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-    assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
-  }
+  Ok(()) 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::{ env, process };
 
-use taro::Config;
+use taro::*;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let config = Config::build(&args).unwrap_or_else(|err| {
+  let config = Config::new(&args).unwrap_or_else(|err| {
     eprintln!("Problem parsing arguments: {err}");
     process::exit(1);
   });
 
-  if let Err(e) = taro::run(config) {
+  if let Err (e) = match config.function {
+      Functions::Grep => {
+        let grep_config = GrepConfig::build(&args).unwrap_or_else(|err| {
+          eprintln!("Problem parsing arguments: {err}");
+          process::exit(1);
+        });
+        taro::run_grep(grep_config)
+      }
+  } {
     eprintln!("Application error: {e}");
-    process::exit(1);
-  };
+    process::exit(1); 
+  }
 }
