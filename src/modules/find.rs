b93e90c64b36c5e9757e2b774982cbc8b257d6b1
Boran Seckin <boran@boranseckin.com>
2022-11-09 15:51:39 -0500

    Fix clippy warnings

--- src/modules/find.rs
+++ src/modules/find.rs
@@ -10,24 +10,22 @@ fn traverse(path: &Path, query: &str, depth: usize, i: usize) -> i32 {
   match fs::read_dir(path) {
     Err(e) => println!("Cannot read directory {:?}: {}", path.file_name().unwrap(), e),
     Ok(entries) => {
-      for entry in entries {
-        if let Ok(entry) = entry {
-          if entry.file_name().to_str().unwrap().contains(query) {
-            println!("{}", entry.path().to_str().unwrap());
-            found_count += 1;
-          }
+      for entry in entries.flatten() {
+        if entry.file_name().to_str().unwrap().contains(query) {
+          println!("{}", entry.path().to_str().unwrap());
+          found_count += 1;
+        }
 
-          if let Ok(metadata) = entry.metadata() {
-            if metadata.is_dir() {
-              found_count += traverse(&entry.path(), query, depth, i + 1);
-            }
+        if let Ok(metadata) = entry.metadata() {
+          if metadata.is_dir() {
+            found_count += traverse(&entry.path(), query, depth, i + 1);
           }
         }
       }
     }
   };
 
-  return found_count;
+  found_count
 }
 
 pub fn run_find(config: FindConfig) -> Result<(), Box<dyn Error>> {
