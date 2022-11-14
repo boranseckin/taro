75467141828384565093cf67b6434d348ccd1eb5
Boran Seckin <boran@boranseckin.com>
2022-11-14 15:00:28 -0500

    Fix wrong indentation

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,27 @@ fn main() {
   });
 
   if let Err (e) = match config.function {
-      Functions::Grep => {
-        let grep_config = GrepConfig::build(&args).unwrap_or_else(|err| {
-          eprintln!("Problem parsing arguments: {err}");
-          process::exit(1);
-        });
-        taro::grep::run_grep(grep_config)
-      },
-      Functions::Find => {
-        let find_config = FindConfig::build(&args).unwrap_or_else(|err| {
-          eprintln!("Problem parsing arguments: {err}");
-          process::exit(1);
-        });
-        taro::find::run_find(find_config)
-      },
-      Functions::GitIgnore => {
-        let gitignore_config = GitIgnoreConfig::build(&args).unwrap_or_else(|err| {
-          eprintln!("Problem parsing arguments: {err}");
-          process::exit(1);
-        });
-        taro::gitignore::run_gitignore(gitignore_config)
-      }
+    Functions::Grep => {
+      let grep_config = GrepConfig::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+      });
+      taro::grep::run_grep(grep_config)
+    },
+    Functions::Find => {
+      let find_config = FindConfig::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+      });
+      taro::find::run_find(find_config)
+    },
+    Functions::GitIgnore => {
+      let gitignore_config = GitIgnoreConfig::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+      });
+      taro::gitignore::run_gitignore(gitignore_config)
+    }
   } {
     eprintln!("Application error: {e}");
     process::exit(1); 
